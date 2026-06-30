fa23951ef64d105157ff0abe9d5c20eaa1433a0a
Ayontika Pal <ayontikapal@gmail.com>
2026-06-30 19:33:27 +0530

    initial commit

---
+++ app/companions/[id]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const CompanionSession=()=>{
+    return(
+        <div>Companion Session
+        </div>
+    )
+}
+
+export default CompanionSession
---
+++ app/companions/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CompanionLibrary=()=>{
+    return(
+        <div>Companions Library</div>
+    )
+}
+
+export default CompanionLibrary
